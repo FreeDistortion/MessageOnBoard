480a5894a3c3586a25ba70e90da0b130627ef42c
FreeDistortion <clouderneering@gmail.com>
2024-01-22 09:14:37 +0900

    1st commit
    Early commit

---
+++ src/main/java/kr/co/koreasign/messageonboard/MessageOnBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.co.koreasign.messageonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageOnBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageOnBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/koreasign/messageonboard/MessageOnBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.koreasign.messageonboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageOnBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
